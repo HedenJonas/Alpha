a70d42583b760f73b9d5e380a18be6ac777c575c
Jonas.Heden <heden.jonas@gmail.com>
2025-03-10 16:15:42 +0100

    Add project files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
